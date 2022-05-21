a9286b4cc68fcf343b7f199d1c7cdbbef665a967
Nicolas Manrique <nicolas.manrique@cnaf.fr>
2022-05-21 22:26:08 +0200

    ethersjs

--- src/TokenCreatorApp.js
+++ src/TokenCreatorApp.js
@@ -42,7 +42,8 @@ let createToken = async () => {
     return;
   }
 
-  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  await provider.send("eth_requestAccounts", []);
   const signer = provider.getSigner();
 
   console.log("Signer's address :", signer.address);
